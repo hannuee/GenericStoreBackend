fba1241416adf35065c928af870fce38879270d4
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-07-07 13:45:31 +0300

    Added middleware, unknown endpoint

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const cors = require('cors')
 const helloRouter = require('./controllers/hello')
+const { unknownEndpoint } = require('./utils/middleware')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.use('/api/hello', helloRouter)
+
+app.use(unknownEndpoint)
 
 module.exports = app
