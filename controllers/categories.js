9aa8bc1ab8f17a5b663c9b246149ee49ef733b0a
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-07-10 19:36:09 +0300

    Basics for some of the core features 3

--- controllers/categories.js
+++ controllers/categories.js
@@ -10,4 +10,21 @@ router.get('/', async (request, response) => {
   }
 })
 
+router.post('/', async (request, response) => {
+  const categoryToAdd = request.body
+
+  const categoryInsertResult = await database.query('INSERT INTO public.Category(category_id, name) VALUES($1, $2) RETURNING id', [categoryToAdd.parentCategoryId, categoryToAdd.name])
+  if(categoryInsertResult.rows.length !== 1) console.log('FAILinsert')
+})
+
+router.delete('/:id', async (request, response) => {
+  categoryIdToDelete = request.params.id
+  try {
+    const result = await database.query('DELETE FROM public.Category WHERE id = $1', [request.params.id])
+    console.log(result)
+  } catch (err) {
+    response.json(err)
+  }
+})
+
 module.exports = router
