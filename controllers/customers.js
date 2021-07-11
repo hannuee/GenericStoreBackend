ff86f6ab21b127aba58f8af2ff54ee5455442037
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-07-11 18:30:51 +0300

    Basics for some of the core features 6
    Installed bcrypt.

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const database = require('../database')
+const bcrypt = require('bcrypt')
 
 router.get('/', async (request, response) => {
     try {
@@ -17,6 +18,18 @@ router.get('/:email', async (request, response) => {
   } catch (err) {
     response.json(err)
   }
+})
+
+router.post('/', async (request, response) => {
+  const customerToAdd = request.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(customerToAdd.password, saltRounds)
+
+  const text = 'INSERT INTO public.Customer(name, address, mobile, email, passwordHash) VALUES($1, $2, $3, $4, $5) RETURNING id'
+  const values = [customerToAdd.name, customerToAdd.address, customerToAdd.mobile, customerToAdd.email, passwordHash]
+  const customerInsertResult = await database.query(text, values)
+  if(customerInsertResult.rows.length !== 1) console.log('FAILinsert')
 })
 
 module.exports = router
