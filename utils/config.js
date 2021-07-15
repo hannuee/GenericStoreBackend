99c75ef21405603973c9a8ad75682e00c33bff31
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-07-15 23:07:58 +0300

    Testing preparations
    Installed jest.
    Installed cross-env.
    Installed supertest.

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 
 let PORT = process.env.PORT
-let DATABASE_URL = process.env.DATABASE_URL
+
+let DATABASE_URL
+if(process.env.NODE_ENV === 'production') DATABASE_URL = process.env.DATABASE_URL
+else if(process.env.NODE_ENV === 'development') DATABASE_URL = process.env.DATABASE_URL
+else if(process.env.NODE_ENV === 'test') DATABASE_URL = process.env.DATABASE_URL
 
 module.exports = {
     PORT,
