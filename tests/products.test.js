3408c0f94b5bab53405defe594bbb04995831326
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-07-16 21:05:36 +0300

    Tests for products router

--- tests/products.test.js
+++ tests/products.test.js
@@ -36,7 +36,95 @@ test('productsGETavailableOfCategory-endpoint returns all available products of 
 
     expect(response.body).toHaveLength(1)
     expect(response.body.find(product => product.name === 'Nissan Micra')).toBeUndefined();
-}) 
+})
+
+test('productsPOST-endpoint adds new product succesfully', async () => {
+    const newCar = {
+        "parentCategoryId": 2,
+        "name": "Toyota Yaris GR",
+        "description": "Small sports car",
+        "pricesAndSizes": [ { "price": 4800000, "size": "" } ],
+        "available": true
+    }
+    
+    const response = await api
+    .post('/api/products/')
+    .send(newCar)
+    .expect(200)
+
+
+    const response2 = await api
+    .get('/api/products/ofCategory/2')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+    expect(response2.body).toHaveLength(1)
+    expect(response2.body.find(product => product.name === 'Toyota Yaris GR')).toBeDefined()
+})
+
+test('productsPUTavailable-endpoint modifies availability status of a product', async () => {
+    const newStatus = {
+        "id": 2,
+        "available": true
+    }
+    
+    const response = await api
+    .put('/api/products/available')
+    .send(newStatus)
+    .expect(200)
+
+
+    const response2 = await api
+    .get('/api/products/availableOfCategory/1')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+    expect(response2.body).toHaveLength(2)
+    expect(response2.body.find(product => product.name === 'Nissan Micra' && product.available)).toBeDefined()
+})
+
+test('productsPUTnewCategory-endpoint modifies parent category of a product', async () => {
+    const newCategory = {
+        "id": 3,
+        "parentCategoryId": 2
+    }
+    
+    const response = await api
+    .put('/api/products/newCategory')
+    .send(newCategory)
+    .expect(200)
+
+
+    const response2 = await api
+    .get('/api/products/OfCategory/2')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+    expect(response2.body).toHaveLength(2)
+    expect(response2.body.find(product => product.name === 'Toyota Landcruiser')).toBeDefined()
+})
+
+test('productsPUTpricesAndSizes-endpoint modifies pricesAndSizes info of a product', async () => {
+    const newPricesAndSizes = {
+        "id": 1,
+        "pricesAndSizes": [ { "price": 3500000, "size": "5 seater" } ]
+    }
+    
+    const response = await api
+    .put('/api/products/pricesAndSizes')
+    .send(newPricesAndSizes)
+    .expect(200)
+
+
+    const response2 = await api
+    .get('/api/products/OfCategory/1')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+    expect(response2.body).toHaveLength(2)
+    expect(response2.body.find(product => product.name === 'Nissan Leaf' && product.pricesandsizes.arr[0].price === 3500000))
+    .toBeDefined()
+})
 
 afterAll(async () => {
     await database.endPool()
