f81bb0269123aae9a4f12c4f170d494df204e795
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-07-18 16:04:31 +0300

    Tests for customers router

--- tests/customers.test.js
+++ tests/customers.test.js
@@ -36,7 +36,50 @@ test('customersGET-endpoint with id returns one customer with all information, b
     expect(customer.passwordHash).toBeUndefined()
 })
 
+test('customersPOST-endpoint adds a new customer succesfully', async () => {
+    const newCustomer = {
+        name: "Sanna",
+        address: "Helsinki",
+        mobile: "045 1234567",
+        email: "sanna@hel.fi",
+        password: "Super Uber passu"
+    }
+    
+    const response = await api
+    .post('/api/customers/')
+    .send(newCustomer)
+    .expect(200)
 
+    // Let's make another HTTP request to verify that the pervious one was succesful:
+    const response2 = await api
+    .get('/api/customers/3')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+    const customer = response2.body
+    expect(customer.id).toBe(3)
+    expect(customer.name).toBe('Sanna')
+    expect(customer.address).toBe('Helsinki')
+    expect(customer.mobile).toBe('045 1234567')
+    expect(customer.email).toBe('sanna@hel.fi')
+    expect(customer.passwordHash).toBeUndefined()
+})
+
+test('customersPOSTlogin-endpoint logs in a customer succesfully', async () => {
+    const customerLoginInfo = {
+        email: "sanna@hel.fi",
+        password: "Super Uber passu"
+    }
+    
+    const response = await api
+    .post('/api/customers/login')
+    .send(customerLoginInfo)
+    .expect(200)
+
+    const customer = response.body
+    expect(customer.token.length > 20).toBe(true)
+    expect(customer.name).toBe('Sanna')
+})
 
 afterAll(async () => {
     await database.endPool()
