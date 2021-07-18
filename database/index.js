31e115652bc59f8dbe37dcd721e6ece105053fec
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-07-18 20:32:54 +0300

    Travis CI testing

--- database/index.js
+++ database/index.js
@@ -89,8 +89,8 @@ const testDataInsertStatements = [
 ]
 
 const clearDatabaseIfNotEmpty = async () => {
-  const countResult = await pool.query('SELECT count(*) FROM pg_stat_user_tables WHERE schemaname = \'public\'')
-  if(countResult.rows[0].count === 0) return
+  //const countResult = await pool.query('SELECT count(*) FROM pg_stat_user_tables WHERE schemaname = \'public\'')
+  //if(countResult.rows[0].count === 0) return
 
   for(let statement of destructDatabaseStatements){
     await pool.query(statement)
